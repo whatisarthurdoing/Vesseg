dc3fcab098e6a131943d663be64f65a509e4e407
Arthur Joschka von der Heyden <92376936+whatisarthurdoing@users.noreply.github.com>
2022-06-03 11:10:35 +0200

    added change form on click

--- app/frontend/src/components/Main.js
+++ app/frontend/src/components/Main.js
@@ -1,54 +1,84 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import "./CSS/Main.css";
 import { FormControl, TextField, Button} from '@mui/material';
 import { Link } from 'react-router-dom';
 //import CopyrightIcon from '@mui/icons-material/Copyright';
 //import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
-/*
-const Title = () => {
-    const [title, setTitle] = useState("Click here");
-    return <h1 onClick={() => setTitle("Hat geklappt")}>{title}</h1>;
+
+function checkEmail(){
+
 }
 
-function formular(){
-    const [title, setTitle] = useState("Sign in");
-    const [name, setName] = useState();
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const [button, setButton] = useState();
-    const [alternative, setAlternative] = useState();
-}
-*/
+
+
 export default function Main() {
-  return (
-    <div className='main'>
-        <div className='split left'>
-            <div className='centered'>
-                <h2>This is vesseg, a tool to speed up atherosclerosis research.</h2>
-                <p>
-                Its is being developed by the Computational Radiology Group at the German Cancer Research Center (DKFZ) in Heidelberg, Germany,
-                in collaboration with the Research Group for Perioperative Vascular Biology at the Clinic for Anaesthesiology
-                at the University Hospital Heidelberg and the Institute for Artificial Intelligence in Medicine (IKIM) at the University Hospital Essen.
-                </p>
-            </div>
-        </div>
+    //Start: Change form on click
+    function useToggle( initialValue = false){
+        const [value, setValue] = useState(initialValue)
 
-        <div className='split right'>
-            <div className='centered'>
-                <h2>Sign in</h2>
+        const toggle = useCallback(() => {
+        setValue(v => !v);
+        }, []);
+        return [value, toggle];
+    }
+
+    const [isOn, toggleIsOn ] = useToggle();
+
+    const [formSignIn, setFormSignIn] = useState({
+        title: "Sign In",
+        name: true, 
+        confirmPassword: true, 
+        button: "SIGN IN",
+        forgotPassword: "Forgot password?", 
+        confirmAccount: "Don't have an account? Sign Up"
+    });
+    const [formSignUp, setFormSignUp] = useState({
+        title: "Sign Up", 
+        name: false, 
+        confirmPassword: false, 
+        button: "SIGN UP", 
+        forgotPassword: "Forgot password?", 
+        confirmAccount: "Have an Account already? Sign in"
+    });
+    //End: Change form on click
+
+
+    return (
+        <div className='main'>
+            <div className='split left'>
+                <div className='centered'>
+                    <h2>This is vesseg, a tool to speed up atherosclerosis research.</h2>
+                    <p>
+                    Its is being developed by the Computational Radiology Group at the German Cancer Research Center (DKFZ) in Heidelberg, Germany,
+                    in collaboration with the Research Group for Perioperative Vascular Biology at the Clinic for Anaesthesiology
+                    at the University Hospital Heidelberg and the Institute for Artificial Intelligence in Medicine (IKIM) at the University Hospital Essen.
+                    </p>
+                </div>
+            </div>
+
+            <div className='split right'>
+                <div className='centered'>
+                <h2>{isOn ? formSignIn.title : formSignUp.title}</h2>
                 <FormControl>
-                    <TextField margin='dense' id="filled-textarea" label="E-Mail *" placeholder='E-Mail *' multilinevariant="filled"/>
-                    <TextField margin='dense' id="filled-textarea" label="Password *" placeholder='Password *' multilinevariant="filled"/>
-                    <Button style={{backgroundColor:'#2F3747'}} variant="contained">SIGN IN</Button>
+                    <TextField required="true" margin='dense' label='Name' placeholder='Name' multilinevariant='filled' disabled = {isOn ? formSignIn.name : formSignUp.name}/>
+                    <TextField required="true" margin='dense' label="E-Mail" inputProps = "email" placeholder='E-Mail' multilinevariant="filled"/>
+                    <TextField required="true" margin='dense' label="Password" placeholder='Password' multilinevariant="filled"/>
+                    <TextField required="true" margin='dense' label="Confirm" placeholder='Confirm password' multilinevariant="filled" disabled={isOn ? formSignIn.confirmPassword : formSignUp.confirmPassword}/>
+                    <Button style={{backgroundColor:'#2F3747'}} variant="contained">{isOn ? formSignIn.button : formSignUp.button}</Button>
                     <div className='alternative'>
-                        <Link to='/forgotpassword'>Forgot password?</Link>
-                        <Link to="/">Don't have an account? Sign Up</Link>
+                        <Link to='/forgotpassword'>{isOn ? formSignIn.forgotPassword : formSignUp.forgotPassword}</Link>
+                        <Link 
+                        to="/"
+                        onClick={toggleIsOn}
+                        >
+                        {isOn ? formSignIn.confirmAccount : formSignUp.confirmAccount}
+                        </Link>
                     </div>
                     <p>Copyright IKIM 2022</p>
                 </FormControl>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
