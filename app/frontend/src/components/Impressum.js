f6dd9a0a28736720d341576be4ccac9b8d63f177
Arthur Joschka von der Heyden <92376936+whatisarthurdoing@users.noreply.github.com>
2022-07-29 08:41:15 +0200

    added impressum

--- app/frontend/src/components/Impressum.js
+++ app/frontend/src/components/Impressum.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import './CSS/Impressum.css';
 
 export default function Impressum() {
   return (
-    <div>Impressum</div>
+    <div id='impressum'>
+      <h1 id='titleImpressum'>Impressum</h1>
+      <p className='textImpressum'>
+        If you have any questions or feedback, please direct them to 
+      </p>
+      <p className='textImpressum'>jacobmatthewmurray at gmail dot com</p>
+    </div>
   )
 }
