2cf06a6337e72e59cc50bf646df46290ae81ee84
Arthur Joschka von der Heyden <92376936+whatisarthurdoing@users.noreply.github.com>
2022-08-01 12:24:18 +0200

    fiexed input validation

--- app/frontend/src/components/Project.js
+++ app/frontend/src/components/Project.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { useParams } from 'react-router-dom';
 import {Paper, Box, TextField, Button} from '@mui/material';
 
 import './CSS/Project.css'
-import { useParams } from 'react-router-dom';
 
 
 
@@ -14,45 +14,102 @@ export default function Project() {
   const {name} = useParams();
   const [title, setTitle] = useState({name});
   const [pageTitle, setPageTitle] = useState(title.name);
-  console.log(pageTitle);
   const [projectId, setProjectId] = useState({id});
+  const [inputText, setInputText] = useState("");
+  const validity = useRef(false);
 
   const patchProject = () => {
-    const requestOptions = {
-      method: "PATCH", 
-      headers: {
-        'Content-Type': 'application/json', 
-        Authorization: "Bearer " + token
-      }, 
-      body: JSON.stringify({
-        title: title
-      })
-    };
-    try{
-      const element = id;
-      const text = "/projects/id"
-      const fetcher = text.replace("id", element)
-      fetch(fetcher, requestOptions)
-      .then(response => response.json())
-      .then(data => setPageTitle(data.title));
+    validate(inputText);
+    if (validity.current){
+      const requestOptions = {
+        method: "PATCH", 
+        headers: {
+          'Content-Type': 'application/json', 
+          Authorization: "Bearer " + token
+        }, 
+        body: JSON.stringify({
+          title: inputText
+        })
+      };
+      try{
+        const element = id;
+        const text = "/projects/id"
+        const fetcher = text.replace("id", element)
+        fetch(fetcher, requestOptions)
+        .then(response => response.json())
+        .then(data => setPageTitle(data.title));
+      }
+      catch(e){
+        console.log(e);
+      }
     }
-    catch(e){
-      console.log(e);
-    }
-    console.log(pageTitle);
   };
 
-  //TODO: Dont change name to a project name that already exists
-  const validate = (inputText) => {
+  const [currentProjects, setCurrentProjects] = useState();
+
+
+  useEffect(() => { 
+    const fetchProjects = async () => {
+      const requestOptions = {
+        method: "GET", 
+        headers: {
+          "content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        }
+      };
+      try{
+        fetch("/projects/", requestOptions)
+        .then((data) => data.json())
+        .then((data) => setCurrentProjects(data));
+      }
+      catch(e){
+        console.log(e)
+      }
+    }
+    fetchProjects();
+  }, []);
+
+  const [helperText, setHelperText] = useState("");
+  const [error, setError] = useState(false);
+
+  function validate(inputText){
+    // Eliminating all whitespaces in input
     const text = inputText.replace(/\s+/g, '');
+    let alreadyExists = false;
+
+    //compare if new project name already exists
+    for( let i = 0; i < currentProjects.length; i++){
+      let projectToCompare = currentProjects[i].title
+      if(projectToCompare === text){
+        alreadyExists = true;
+      }
+    }
+
+    // Input is empty
     if (text === ''){
-      //TODO: Error Message: No Whitespaces Allowed
-      setTitle("No Spaces allowed")
+      setHelperText("No whitespaces allowed");
+      setError(true);
+      validity.current = false;
+    }
+    // Input is too short or too long
+    else if(text.length < 4 || text.length > 30){
+      setHelperText("Name must be between 4 and 30 characters long");
+      setError(true);
+      validity.current = false;
+    }
+    // new project name already exists
+    else if(alreadyExists){
+      setHelperText("There is already a project with this name");
+      setError(true);
+      validity.current = false;
     }
     else{
-      setTitle(text);
+      setInputText(text);
+      setError(false);
+      validity.current = true;
     }
-  };
+  }
+
 
   return (
     <div className='singleProject'>
@@ -65,7 +122,9 @@ export default function Project() {
               label="Name"
               type="text"
               variant='standard'
-              onChange={(v) => validate(v.target.value)}
+              error = {error}
+              helperText = {error ? helperText : ""}
+              onChange={(v) => setInputText(v.target.value)}
             />
             <Button className="buttons" style={{color:'#2F3747'}} variant="text" onClick={patchProject}>Change project name</Button>
           </div>
