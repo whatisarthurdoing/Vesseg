7432287ea9a5fb9a12762fdc7232a2e650379f82
Arthur Joschka von der Heyden <92376936+whatisarthurdoing@users.noreply.github.com>
2022-08-21 16:43:05 +0200

    added color when button is active

--- app/frontend/src/components/Header.js
+++ app/frontend/src/components/Header.js
@@ -1,9 +1,17 @@
 import { AppBar, Toolbar, Typography, Button, Link} from "@mui/material";
 import React, {useContext, useState} from "react";
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 
 import './CSS/Header.css'
 import { UserContext } from '../context/UserContext';
+
+//TODO: Add "Task" buton between projects and about connected to full-screen dialog:
+//https://mui.com/material-ui/react-dialog/
+
+
+//TODO: add active color: rgb(219, 112, 147)/#db7093
 
 export default function Header() {
   const displayDesktop = () => {
@@ -22,7 +30,25 @@ export default function Header() {
     </Button>
   );
 
+  const [buttonColor, setButtonColor] = useState("secondary");
   const [flag, setFlag] = useState(true);
+
+  /*
+  const handleClick = () => {
+    setFlag(!flag);
+  };
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#db7093'
+      }, 
+      secondary:{
+        main: '#ffffff'
+      }
+    }
+  })
+  */
 
   const getMenuButton = () => {
 
@@ -30,16 +56,77 @@ export default function Header() {
 
     if (token === null){
       return(
-        <Button href="/about" sx={{color: flag ? "inherit" : "primary"}} onClick={ () => setFlag(!flag)}>About</Button>
+        <Button 
+          href="/about" 
+          sx={{
+            '&:active': {
+              color: '#db7093',
+              backgroundColor: '#2F3747'
+            }, 
+            color: '#ffffff',
+            backgroundColor: '#2F3747'
+          }}
+        >
+          About
+        </Button>
       );
     }
     else{
       return(
         <div id="contentButtons">
-          <Button href="/projects" color={flag ? "inherit" : "primary"} onClick={ () => setFlag(!flag)}>Projects</Button>
-          <Button href="/about" color={flag ? "inherit" : "primary"} onClick={ () => setFlag(!flag)}>About</Button>
-          <Button href="/faq" color={flag ? "inherit" : "primary"} onClick={ () => setFlag(!flag)}>FAQ</Button>
-          <Link href="/settings" onClick={ () => setFlag(!flag)} color={flag ? "inherit" : "primary"} ><SettingsOutlinedIcon/></Link>
+          <Button 
+            href="/projects" 
+            sx={{
+              '&:active': {
+                color: '#db7093',
+                backgroundColor: '#2F3747'
+              }, 
+              color: '#ffffff',
+              backgroundColor: '#2F3747'
+            }}
+            //onClick={handleClick}
+          >
+            Projects
+          </Button>
+          <Button 
+            href="/about" 
+            sx={{
+              '&:active': {
+                color: '#db7093',
+                backgroundColor: '#2F3747'
+              }, 
+              color: '#ffffff',
+              backgroundColor: '#2F3747'
+            }}
+          >
+            About
+          </Button>
+          <Button 
+            href="/faq" 
+            sx={{
+              '&:active': {
+                color: '#db7093',
+                backgroundColor: '#2F3747'
+              }, 
+              color: '#ffffff',
+              backgroundColor: '#2F3747'
+            }}
+          >
+            FAQ
+          </Button>
+          <Link 
+            href="/settings" 
+            sx={{
+              '&:active': {
+                color: '#db7093',
+                backgroundColor: '#2F3747'
+              }, 
+              color: '#ffffff',
+              backgroundColor: '#2F3747'
+            }}
+          >
+            <SettingsOutlinedIcon/>
+          </Link>
         </div>
       );
     }
