8d8bea9f6c2d69e571bc208a3ba9b915469bb5eb
Arthur Joschka von der Heyden <92376936+whatisarthurdoing@users.noreply.github.com>
2022-10-02 10:30:41 +0200

    changed fonts

--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -3,15 +3,35 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import { createTheme } from '@mui/material';
+import { ThemeProvider } from '@mui/system';
 
 import {UserProvider} from './context/UserContext';
+
+
 const container = document.getElementById('root');
 const root = createRoot(container);
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Helvetica',
+      'Arial',
+      'Nunito',
+      'Roboto',
+      'Helvetica Neue',
+      'sans-serif'
+    ].join(','),
+  }
+});
+
 root.render(
-  <BrowserRouter>
-    <UserProvider>
-      <App />
-    </UserProvider>
-  </BrowserRouter>
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>
+  </ThemeProvider>
 )
 
